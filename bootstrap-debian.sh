f376efedfab6a602c19b38a7422ff6c45f20d4ce
Michael Krieg <info@red-tag.de>
2014-08-07 09:55:42 +0200

    removed local hosts

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -3,8 +3,6 @@
 apt-get update -qq
 [ -x /usr/bin/git ] || apt-get install -y -q git
 [ -x /opt/vagrant_ruby/bin/r10k ] || gem install --no-rdoc --no-ri r10k
-egrep "zitrone2" /etc/hosts || echo "10.10.39.2 zitrone2.red-tag.de" >> /etc/hosts
-egrep "zitrone2" /etc/ssh/ssh_config || echo -e "Host zitrone2.red-tag.de\n\tHostname 10.10.39.2\n\tStrictHostKeyChecking no\n\tPort 20022\n\tUser git" >> /etc/ssh/ssh_config
 
 cd /vagrant && r10k -v info puppetfile install
 
