17272e52f7030764b6401a5228786cce053f327a
David Lowes <david.l@zend.com>
2014-09-05 15:19:42 +0300

    local testing changes

--- local_modules/zstestbox/manifests/init.pp
+++ local_modules/zstestbox/manifests/init.pp
@@ -1,3 +1,23 @@
 class zstestbox {
   class {'::zendserver': }
+  file {'/tmp/mtrig.zpk':
+    ensure => present,
+    source => "puppet:///modules/${module_name}/mtrig.zpk",
+  }
+
+  zendserver::application { 'sanity':
+    ensure      => 'deployed',
+    app_package => '/tmp/mtrig.zpk',
+    require       => [Zendserver::Sdk::Target['localadmin'],File['/tmp/mtrig.zpk']],
+  }
+
+  zendserver::sdk::target { 'localadmin':
+    zskey => $::zend_api_key_name,
+    zssecret => $::zend_api_key_hash,
+  }
+
+  file {'/root/.zsapi.ini':
+    ensure => link,
+    target => '/.zsapi.ini',
+  }
 }
