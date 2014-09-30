013b9d7bb0d3488c4fefefc0995c74f825fe5282
David Lowes <david.l@zend.com>
2014-09-30 19:49:28 +0300

    Added support for Debian

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+#Get latest puppet version on Debian since it comes with an incompatible version
+if [ -f /etc/debian_version ] ; then
+  DistroBasedOn='Debian'
+  DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
+  PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+  REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+  pushd /tmp
+  wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb && \
+  sudo dpkg -i puppetlabs-release-precise.deb
+  popd
+fi
+
 apt-get update -qq
+apt-get install -y -q puppet
 [ -x /usr/bin/git ] || apt-get install -y -q git
 [ -x /opt/vagrant_ruby/bin/r10k ] || gem install --no-rdoc --no-ri r10k
 
